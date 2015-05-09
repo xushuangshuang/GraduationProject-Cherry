63cd8973f38cca8659ffe43cfeab6a2ccf6e658d
xushuangshuang <xushuangshuang1@gmail.com>
2015-05-09 17:31:00 +0800

    增加了weatcher icon 的相关文件

---
+++ app/src/main/java/com/xushuangshuang/graduation/cherry/util/ResourceClass.java
@@ -0,0 +1,62 @@
+package com.xushuangshuang.graduation.cherry.util;
+
+import com.xushuangshuang.graduation.cherry.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/5/9.
+ */
+public class ResourceClass {
+
+    public static List<Integer> getWeatherIconArray() {
+        List<Integer> integerList = new ArrayList<Integer>() {
+            {
+                add(R.drawable.sunny_0);
+                add(R.drawable.clear_1);
+                add(R.drawable.fair_2);
+                add(R.drawable.fair_3);
+                add(R.drawable.cloudy_4);
+                add(R.drawable.partly_cloudy_5);
+                add(R.drawable.partly_cloudy_6);
+                add(R.drawable.mostly_cloudy_7);
+                add(R.drawable.mostly_cloudy_7);
+                add(R.drawable.mostly_cloudy_8);
+                add(R.drawable.overcast_9);
+                add(R.drawable.shower_10);
+                add(R.drawable.thundershower_11);
+                add(R.drawable.thundershower_with_hail_12);
+                add(R.drawable.light_rain_13);
+                add(R.drawable.moderate_rain_14);
+                add(R.drawable.heavy_rain_15);
+                add(R.drawable.storm_16);
+                add(R.drawable.heavy_storm_17);
+                add(R.drawable.severe_storm_18);
+                add(R.drawable.ice_rain_19);
+                add(R.drawable.sleet_20);
+                add(R.drawable.snow_flurry_21);
+                add(R.drawable.light_snow_22);
+                add(R.drawable.moderate_snow_23);
+                add(R.drawable.heavy_snow_24);
+                add(R.drawable.snowstorm_25);
+                add(R.drawable.dust_26);
+                add(R.drawable.sand_27);
+                add(R.drawable.duststorm_28);
+                add(R.drawable.sandstorm_29);
+                add(R.drawable.foggy_30);
+                add(R.drawable.haze_31);
+                add(R.drawable.windy_32);
+                add(R.drawable.blustery_33);
+                add(R.drawable.hurricane_34);
+                add(R.drawable.tropical_storm_35);
+                add(R.drawable.tornado_36);
+                add(R.drawable.cold_37);
+                add(R.drawable.hot_38);
+                add(R.drawable.unknown_99);
+            }
+        };
+        return integerList;
+    }
+
+}
